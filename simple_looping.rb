d50c4bf31a503659ae009d1c79804ba436c73f51
Dale Sublett <djsublett04@gmail.com>
2019-04-07 22:15:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ end
 
 def times_iterator(number_of_times)
    7.times do
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+   phrase = "Welcome to Flatiron School's Web Development Course!"
 
    puts phrase
   end
